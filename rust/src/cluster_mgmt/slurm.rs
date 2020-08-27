386b5b2ab8c50294b9e3202834badbdd4f79de54
Simon Heath <simon.heath@gmail.com>
2020-08-27 15:55:53 +0200

    Clean up slurm log files as last stage

--- rust/src/cluster_mgmt/slurm.rs
+++ rust/src/cluster_mgmt/slurm.rs
@@ -59,11 +59,31 @@ fn write_sbatch_script(mut file: &fs::File, jv: &SlurmJob, tl: &TaskList,  optio
 	Ok(())
 }
 
-// Prepare job graph and submit to slurm
-pub fn handle_slurm(gem_bs: &GemBS, options: &HashMap<&'static str, DataValue>, task_list: &[usize]) -> Result<(), String> {
+fn write_sbatch_rm_script(mut file: &fs::File, logfiles: &[String]) -> std::io::Result<()> {
+	writeln!(file, "#!/bin/sh")?;
+	write!(file, "for f in")?;
+	for f in logfiles.iter() {
+		write!(file," \\\n {}", f)?;
+	}
+	writeln!(file,"\ndo\n rm -f slurm_logs/slurm_gemBS-${{f}}.out\ndone\necho Pipeline terminated successfully")
+}
+
+fn get_slurm_id(mut file: fs::File) -> Result<usize, String> {
 	lazy_static! {
         static ref RE: Regex = Regex::new(r"^Submitted batch job (\d+)").unwrap();
 	}
+	file.seek(SeekFrom::Start(0)).map_err(|e| format!("{}", e))?;
+	let mut content = String::new();
+       file.read_to_string(&mut content).unwrap();
+	if let Some(cap) = RE.captures(content.as_str()) {
+		if let Ok(x) = <usize>::from_str(cap.get(1).unwrap().as_str()) { return Ok(x); }
+	}
+	Err(format!("Could not parse output from sbatch: {}", content))
+}
+
+// Prepare job graph and submit to slurm
+pub fn handle_slurm(gem_bs: &GemBS, options: &HashMap<&'static str, DataValue>, task_list: &[usize]) -> Result<(), String> {
+	let _ = fs::create_dir("slurm_logs");
 	let mut job_vec: Vec<SlurmJob> = Vec::new();
 	let mut slurm_id = Vec::new();
 	let mut job_hash: HashMap<Rc<JobNode>, usize> = HashMap::new();
@@ -97,11 +117,12 @@ pub fn handle_slurm(gem_bs: &GemBS, options: &HashMap<&'static str, DataValue>, 
 	}
 	let verbose = gem_bs.verbose();
 	let sbatch_path = Path::new("sbatch");
+	let mut dep_hash = HashSet::new();
 	for jv in job_vec.iter() {
 		let mut tfile = tempfile::tempfile().expect("Couldn't create temporary slurm input file");
 		write_sbatch_script(&tfile, jv, gem_bs.get_tasks(), options, verbose).map_err(|e| format!("Error writing sbatch script: {}", e))?;
 		tfile.seek(SeekFrom::Start(0)).map_err(|e| format!("{}", e))?;
-		let mut ofile = tempfile::tempfile().expect("Couldn't create temporary slurm output file");
+		let ofile = tempfile::tempfile().expect("Couldn't create temporary slurm output file");
 		let mut sbatch_args = Vec::new();
 		let mut hs = HashSet::new();
 		let mut desc = String::from("gemBS");
@@ -115,7 +136,10 @@ pub fn handle_slurm(gem_bs: &GemBS, options: &HashMap<&'static str, DataValue>, 
 		sbatch_args.push(format!("--cpus-per-task={}", jv.node.cores));
 		sbatch_args.push(format!("--mem={:#}", jv.node.mem));
 		sbatch_args.push(format!("--time={}", jv.node.time));
-		if jv.task_vec.len() > 1 { sbatch_args.push(format!("--array=0-{}", jv.task_vec.len() - 1)); }
+		if jv.task_vec.len() > 1 { 
+			sbatch_args.push(format!("--array=0-{}", jv.task_vec.len() - 1)); 
+			sbatch_args.push("--output=slurm_logs/slurm_gemBS-%A_%a.out".to_string());
+		} else { sbatch_args.push("--output=slurm_logs/slurm_gemBS-%j.out".to_string()); }
 		if !jv.node.depend.is_empty() {
 			let mut t = String::from("--dependency=afterok");
 			for ix in jv.node.depend.iter() {
@@ -127,6 +151,7 @@ pub fn handle_slurm(gem_bs: &GemBS, options: &HashMap<&'static str, DataValue>, 
 					format!(":{}", slurm_job_id)
 				};
 				t.push_str(job_id.as_str());
+				dep_hash.insert(ix);
 			}
 			sbatch_args.push(t);
 		}
@@ -135,17 +160,41 @@ pub fn handle_slurm(gem_bs: &GemBS, options: &HashMap<&'static str, DataValue>, 
 				.in_file(tfile)
 				.out_file(ofile.try_clone().expect("Couldn't clone output file descriptor"));
 		pipeline.run(gem_bs.get_signal_clone())?;
-		ofile.seek(SeekFrom::Start(0)).map_err(|e| format!("{}", e))?;
-		let mut content = String::new();
-        ofile.read_to_string(&mut content).unwrap();
-		let mut fg = false;
-		if let Some(cap) = RE.captures(content.as_str()) {
-			if let Ok(x) = <usize>::from_str(cap.get(1).unwrap().as_str()) {
-				slurm_id.push(x);
-				fg = true;
+		slurm_id.push(get_slurm_id(ofile)?);
+	}
+	let mut deps = String::new();
+	let mut logfiles = Vec::new();
+	for (ix, jv) in job_vec.iter().enumerate() {
+		let len = jv.task_vec.len();
+		let job_id = slurm_id[ix];
+		if len == 1 {
+			let sdep = SlurmDep{job_ix: ix, task_ix: 0};
+			if ! dep_hash.contains(&sdep) { deps.push_str(format!(":{}", job_id).as_str()); }
+			logfiles.push(format!("{}", job_id));
+		} else {
+			for i in 0..len {
+				let sdep = SlurmDep{job_ix: ix, task_ix: i};
+				if ! dep_hash.contains(&sdep) { deps.push_str(format!(":{}_{}", job_id, i).as_str()); }
+				logfiles.push(format!("{}_{}", job_id, i));
 			}
 		}
-		if !fg { return Err(format!("Could not parse output from sbatch: {}", content)); }
+	}
+	if ! logfiles.is_empty() {
+		let mut tfile = tempfile::tempfile().expect("Couldn't create temporary slurm input file");
+		write_sbatch_rm_script(&tfile, &logfiles).map_err(|e| format!("Error writing sbatch script: {}", e))?;
+		tfile.seek(SeekFrom::Start(0)).map_err(|e| format!("{}", e))?;
+		let mut sbatch_args = vec!("--job-name=gemBS_clean_logfiles", "--cpus-per-task=1", "--time=10", "--output=slurm_logs/slurm_gemBS_pipeline.out");
+		if ! deps.is_empty() { 
+			deps = format!("--dependency=afterok{}", deps); 
+			sbatch_args.push(deps.as_str());
+		}
+		let ofile = tempfile::tempfile().expect("Couldn't create temporary slurm output file");
+		let mut pipeline = Pipeline::new();		
+		pipeline.add_stage(&sbatch_path, Some(sbatch_args.iter()))
+				.in_file(tfile)
+				.out_file(ofile.try_clone().expect("Couldn't clone output file descriptor"));
+		pipeline.run(gem_bs.get_signal_clone())?;
+		let _ = get_slurm_id(ofile)?;	
 	}
 	Ok(())
 }
