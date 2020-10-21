04b0b1ee88fc5f94953c742a1732d40fea2c6408
Simon Heath <simon.heath@gmail.com>
2020-10-21 15:13:42 +0200

    Optimizations in vcf printing

--- rust/bs_call/src/process/vcf/write_vcf_entry.rs
+++ rust/bs_call/src/process/vcf/write_vcf_entry.rs
@@ -1,7 +1,6 @@
 use std::sync::{Arc, mpsc};
 use std::{cmp, io};
 use std::collections::VecDeque;
-use std::time::{Duration, Instant};
 use crate::config::{BsCallConfig, BsCallFiles};
 use crate::htslib::*;
 use libc::{c_char, c_int};
@@ -34,10 +33,10 @@ const FLT_ID_CG: usize = 13;
 const FLT_ID_CX: usize = 14;
 const FLT_ID_FS: usize = 15;
 
-fn get_filter_ids(hdr: &VcfHeader) -> Vec<c_int> {
+fn get_filter_ids(hdr: &VcfHeader) -> Vec<u8> {
 	let mut v = Vec::with_capacity(FLT_NAMES.len());
 	for flt in FLT_NAMES.iter() {
-		v.push(hdr.id2int(BCF_DT_ID as usize, flt).expect("mac1 filter not found in VCF header") as c_int)
+		v.push(hdr.id2int(BCF_DT_ID as usize, flt).expect("mac1 filter not found in VCF header") as u8)
 	}	
 	v
 }
@@ -163,37 +162,6 @@ fn handle_filters(call: &GenotypeCall, cs: &CallStats) -> (usize, usize) {
 	} else { (flt, FLT_ID_FAIL) }
 }
 
-fn write_fixed_columns(call: &GenotypeCall, hdr: &VcfHeader, filter_ids: &[c_int], call_stats: &CallStats, ref_context: &[u8], bcf_rec: &mut BcfRec) -> io::Result<usize> {
-	// Alternate alleles
-	let alt_alleles = REF_ALT[call.max_gt as usize][call.ref_base as usize];
-	// Fisher strand and quality by depth stats
-	let kstr = bcf_rec.shared();
-	// ID - for the moment leave blank
-	kstr.bcf_enc_size(0, BCF_BT_CHAR);	
-	// REF allele
-	kstr.bcf_enc_size(1, BCF_BT_CHAR);
-	kstr.putc(ref_context[2] as c_char);
-	// ALT alleles
-	for allele in alt_alleles.as_bytes().iter().copied() {
-		kstr.bcf_enc_size(1, BCF_BT_CHAR);
-		kstr.putc(allele as i8);
-	}
-	// QUAL
-	// FILTER
-	let (filter, filter_id) = handle_filters(call, &call_stats);	
-	kstr.bcf_enc_int1(filter_ids[filter_id]);	
-	// INFO
-	kstr.bcf_enc_int1(filter_ids[FLT_ID_CX]);
-	kstr.bcf_enc_vchar(ref_context);	
-
-	bcf_rec.set_n_sample(1);
-	bcf_rec.set_rlen(1);	
-	bcf_rec.set_n_allele(1 + alt_alleles.len());
-	bcf_rec.set_qual(call_stats.phred as f32);	
-	bcf_rec.set_n_info(1);
-	Ok(filter)
-}
-
 fn get_filter_string(filter: usize) -> String {
 	if (filter & 15) != 0 {
 		let mut flt1 = filter & 15;
@@ -232,59 +200,159 @@ fn get_gt_like(call: &GenotypeCall) -> Vec<f32> {
 	v
 }
 
-fn write_format_columns(call: &GenotypeCall, filter_ids: &[c_int], called_context: &[u8], cpg: u8, filter: usize, call_stats: &CallStats, bcf_rec: &mut BcfRec) -> io::Result<()> {
-	let kstr = bcf_rec.indiv();
+fn enc_size(v: &mut Vec<u8>, size: c_int, bcf_type: u8) {
+	if size < 15 { v.push(((size as u8) << 4) | bcf_type) }
+	else {
+		v.push(0xf0 | bcf_type);
+		if size >= 128 {
+			if size >= 32768 {
+				v.push(0x10 | BCF_BT_INT32);
+				v.extend_from_slice(&size.to_le_bytes());
+			} else {
+				v.push(0x10 | BCF_BT_INT16);
+				v.extend_from_slice(&(size as u16).to_le_bytes());
+			}
+		} else {
+			v.push(0x10 | BCF_BT_INT8);
+			v.push(size as u8);
+		}
+	}
+}
+
+// Note - we don't check for vector_end or missing values here as we don't generate these
+fn enc_int(v: &mut Vec<u8>, x: c_int) {
+	if x <= BCF_MAX_BT_INT8 && x >= BCF_MIN_BT_INT8 {
+		enc_size(v, 1, BCF_BT_INT8);
+		v.push(x as u8)
+	} else if x <= BCF_MAX_BT_INT16 && x >= BCF_MIN_BT_INT16 {
+		enc_size(v, 1, BCF_BT_INT16);
+		v.extend_from_slice(&(x as u16).to_le_bytes())							
+	} else {
+		enc_size(v, 1, BCF_BT_INT32);
+		v.extend_from_slice(&(x as u32).to_le_bytes())							
+	}	
+}
+
+#[inline]
+fn enc_u8(v: &mut Vec<u8>, x: u8) {
+	v.push(0x10 | BCF_BT_INT8);
+	v.push(x);
+}
+
+fn enc_vchar(v: &mut Vec<u8>, s: &[u8]) { 
+	enc_size(v, s.len() as c_int, BCF_BT_CHAR);
+	v.extend_from_slice(s) 
+}
+
+fn enc_vfloat(v: &mut Vec<u8>, s: &[f32]) {
+	enc_size(v, s.len() as c_int, BCF_BT_FLOAT);
+	s.iter().copied().for_each(|x| v.extend_from_slice(&x.to_le_bytes()));
+}
+
+fn enc_vint(v: &mut Vec<u8>, s: &[c_int]) {
+	if s.len() == 1 { enc_int(v, s[0])}
+	else {
+		let (min, max): (c_int, c_int) = s[1..].iter().copied().fold((s[0], s[0]), |(mn, mx), x| if x < mn { (x, mx) } else if x > mx { (mn, x) } else { (mn, mx) });
+		if max <= BCF_MAX_BT_INT8 && min >= BCF_MIN_BT_INT8 {
+			enc_size(v, s.len() as c_int, BCF_BT_INT8);
+			s.iter().copied().for_each(|x| v.push(x as u8));
+		} else if max <= BCF_MAX_BT_INT16 && min >= BCF_MIN_BT_INT16 {
+			enc_size(v, s.len() as c_int, BCF_BT_INT16);
+			s.iter().copied().for_each(|x| v.extend_from_slice(&(x as u16).to_le_bytes()));
+		} else {
+			enc_size(v, s.len() as c_int, BCF_BT_INT32);
+			s.iter().copied().for_each(|x| v.extend_from_slice(&(x as u32).to_le_bytes()));							
+		}
+	}		
+}
+
+fn write_fixed_columns(call: &GenotypeCall, filter_ids: &[u8], v: &mut Vec<u8>, call_stats: &CallStats, ref_context: &[u8], bcf_rec: &mut BcfRec) -> io::Result<usize> {
+	v.clear();
+	// Alternate alleles
+	let alt_alleles = REF_ALT[call.max_gt as usize][call.ref_base as usize];
+	// ID - for the moment leave blank
+	v.push(BCF_BT_CHAR);	
+	// REF allele
+	v.push(0x10 | BCF_BT_CHAR);
+	v.push(ref_context[2]);
+	// ALT alleles
+	for allele in alt_alleles.as_bytes().iter().copied() {
+		v.push(0x10 | BCF_BT_CHAR);
+		v.push(allele);
+	}
+	// FILTER
+	let (filter, filter_id) = handle_filters(call, &call_stats);	
+	enc_u8(v, filter_ids[filter_id]);	
+	// INFO
+	enc_u8(v, filter_ids[FLT_ID_CX]);
+	enc_vchar(v, ref_context);	
+
+	let kstr = bcf_rec.shared();
+	kstr.putsn(v.as_slice().as_ptr() as *const c_char, v.len());
+
+	bcf_rec.set_n_sample(1);
+	bcf_rec.set_rlen(1);	
+	bcf_rec.set_n_allele(1 + alt_alleles.len());
+	bcf_rec.set_qual(call_stats.phred as f32);	
+	bcf_rec.set_n_info(1);
+	Ok(filter)
+}
+
+fn write_format_columns(call: &GenotypeCall, filter_ids: &[u8], called_context: &[u8], cpg: u8, filter: usize, v: &mut Vec<u8>, call_stats: &CallStats, bcf_rec: &mut BcfRec) -> io::Result<()> {
 	let mut n_fmt = 11;
+	v.clear();
 	// GT
-	kstr.bcf_enc_int1(filter_ids[FLT_ID_GT]);
-	kstr.bcf_enc_vint(&GT_INT[call.max_gt as usize][call.ref_base as usize]);
+	enc_u8(v, filter_ids[FLT_ID_GT]);
+	enc_vint(v, &GT_INT[call.max_gt as usize][call.ref_base as usize]);
 	// FT
 	let flt_str = get_filter_string(filter);
-	kstr.bcf_enc_int1(filter_ids[FLT_ID_FT]);
-	kstr.bcf_enc_vchar(flt_str.as_bytes());
+	enc_u8(v, filter_ids[FLT_ID_FT]);
+	enc_vchar(v, flt_str.as_bytes());
+
 	// DP
-	kstr.bcf_enc_int1(filter_ids[FLT_ID_DP]);
-	kstr.bcf_enc_int1(call_stats.dp1);
+	enc_u8(v, filter_ids[FLT_ID_DP]);
+	enc_int(v, call_stats.dp1);
 	// MQ
-	kstr.bcf_enc_int1(filter_ids[FLT_ID_MQ]);
-	kstr.bcf_enc_int1(call.mq as c_int);
+	enc_u8(v, filter_ids[FLT_ID_MQ]);
+	enc_int(v, call.mq as c_int);
 	// GQ
-	kstr.bcf_enc_int1(filter_ids[FLT_ID_GQ]);
-	kstr.bcf_enc_int1(call_stats.phred);
+	enc_u8(v, filter_ids[FLT_ID_GQ]);
+	enc_int(v, call_stats.phred);
 	// QD
-	kstr.bcf_enc_int1(filter_ids[FLT_ID_QD]);
-	kstr.bcf_enc_int1(call_stats.qd);
+	enc_u8(v, filter_ids[FLT_ID_QD]);
+	enc_int(v, call_stats.qd);
 	// GL
 	let gl = get_gt_like(call);	
-	kstr.bcf_enc_int1(filter_ids[FLT_ID_GL]);
-	kstr.bcf_enc_vfloat(&gl);
+	enc_u8(v, filter_ids[FLT_ID_GL]);
+	enc_vfloat(v, &gl);
 	// MC8
-//	let v: Vec<c_int> = call.counts.iter().map(|x| *x as c_int).collect();
-	kstr.bcf_enc_int1(filter_ids[FLT_ID_MC8]);
-	kstr.bcf_enc_vint(&call.counts);
+	enc_u8(v, filter_ids[FLT_ID_MC8]);
+	enc_vint(v, &call.counts);
 	// AMQ
-	let v: Vec<c_int> = call.counts.iter().copied().zip(call.qual.iter()).filter(|(c,_)| *c > 0).map(|(_, q)| *q).collect();
-	if !v.is_empty() {
-		kstr.bcf_enc_int1(filter_ids[FLT_ID_AMQ]);
-		kstr.bcf_enc_vint(&v);
+	let tv: Vec<c_int> = call.counts.iter().copied().zip(call.qual.iter()).filter(|(c,_)| *c > 0).map(|(_, q)| *q).collect();
+	if !tv.is_empty() {
+		enc_u8(v, filter_ids[FLT_ID_AMQ]);
+		enc_vint(v, &tv);
 		n_fmt += 1;
 	}
 	// CS
-	kstr.bcf_enc_int1(filter_ids[FLT_ID_CS]);
-	kstr.bcf_enc_vchar(CS_STR[call.max_gt as usize].as_bytes());
+	enc_u8(v, filter_ids[FLT_ID_CS]);
+	enc_vchar(v, CS_STR[call.max_gt as usize].as_bytes());
 	// CG
-	kstr.bcf_enc_int1(filter_ids[FLT_ID_CG]);
-	kstr.bcf_enc_size(1, BCF_BT_CHAR);
-	kstr.putc_(cpg as c_char);
+	enc_u8(v, filter_ids[FLT_ID_CG]);
+	v.push(0x10 | BCF_BT_CHAR);
+	v.push(cpg);
 	// CX
-	kstr.bcf_enc_int1(filter_ids[FLT_ID_CX]);
-	kstr.bcf_enc_vchar(called_context);
+	enc_u8(v, filter_ids[FLT_ID_CX]);
+	enc_vchar(v, called_context);
 	// FS
 	if GT_HET[call.max_gt as usize] {
-		kstr.bcf_enc_int1(filter_ids[FLT_ID_FS]);
-		kstr.bcf_enc_int1(call_stats.fs);
+		enc_u8(v, filter_ids[FLT_ID_FS]);
+		enc_int(v, call_stats.fs);
 		n_fmt += 1;		
 	}
+	let kstr = bcf_rec.indiv();
+	kstr.putsn(v.as_slice().as_ptr() as *const c_char, v.len());
 	bcf_rec.set_n_fmt(n_fmt);
 	Ok(())
 }
@@ -294,6 +362,7 @@ struct WriteState {
 	curr_x: usize,
 	call_buf: VecDeque<CallEntry>,
 	bcf_rec: BcfRec,
+	tvec: Vec<u8>,
 }
 
 impl WriteState {
@@ -302,19 +371,20 @@ impl WriteState {
 		for _ in 0..3 { v.push_back(CallEntry::Starting(0)) }
 		for c in call_block.prec_ref_bases.iter() { v.push_back(CallEntry::Starting(*c)) }
 		let bcf_rec = BcfRec::new().expect("Couldn't allocate Bcf Record");
-		Self { sam_tid: call_block.sam_tid, curr_x: call_block.start, call_buf: v, bcf_rec}
+		let tvec = Vec::with_capacity(256);
+		Self { sam_tid: call_block.sam_tid, curr_x: call_block.start, call_buf: v, bcf_rec, tvec}
 	}
-	fn finish_block(&mut self, bs_cfg: &BsCallConfig, vcf_output: &mut VcfFile, filter_ids: &[c_int], stat_tx: &mpsc::Sender<StatJob>, timing: &mut (Duration, Duration)) -> io::Result<()> {
+	fn finish_block(&mut self, bs_cfg: &BsCallConfig, vcf_output: &mut VcfFile, filter_ids: &[u8], stat_tx: &mpsc::Sender<StatJob>) -> io::Result<()> {
 		for _ in 0..2 {
 			self.add_entry(CallEntry::Skip(0));
-			self.write_entry(bs_cfg, vcf_output, filter_ids, stat_tx, timing)?;
+			self.write_entry(bs_cfg, vcf_output, filter_ids, stat_tx)?;
 		}
 		Ok(())
 	}
-	fn handle_calls(&mut self, mut call_vec: Vec<CallEntry>, bs_cfg: &BsCallConfig, vcf_output: &mut VcfFile, filter_ids: &[c_int], stat_tx: &mpsc::Sender<StatJob>, timing: &mut (Duration, Duration)) -> io::Result<()> {
+	fn handle_calls(&mut self, mut call_vec: Vec<CallEntry>, bs_cfg: &BsCallConfig, vcf_output: &mut VcfFile, filter_ids: &[u8], stat_tx: &mpsc::Sender<StatJob>) -> io::Result<()> {
 		for entry in call_vec.drain(..) {
 			self.add_entry(entry);
-			self.write_entry(bs_cfg, vcf_output, filter_ids, stat_tx, timing)?;
+			self.write_entry(bs_cfg, vcf_output, filter_ids, stat_tx)?;
 		}
 		Ok(())
 	}
@@ -323,7 +393,7 @@ impl WriteState {
 		self.call_buf.push_back(entry);
 		let _ = self.call_buf.pop_front();		
 	}
-	fn write_entry(&mut self, bs_cfg: &BsCallConfig, vcf_output: &mut VcfFile, filter_ids: &[c_int], stat_tx: &mpsc::Sender<StatJob>, timing: &mut (Duration, Duration)) -> io::Result<()> {
+	fn write_entry(&mut self, bs_cfg: &BsCallConfig, vcf_output: &mut VcfFile, filter_ids: &[u8], stat_tx: &mpsc::Sender<StatJob>) -> io::Result<()> {
 		match &self.call_buf[2] {
 			CallEntry::Call(call) => {
 				let all_positions = bs_cfg.conf_hash.get_bool("all_positions");
@@ -351,17 +421,14 @@ impl WriteState {
 				}
 				let called_context: Vec<u8> = called_gt.iter().copied().map(|g| IUPAC.as_bytes()[g]).collect(); 
 				let filter = if !skip {
+					let tvec = &mut self.tvec;
 					let rid = bs_cfg.ctg_vcf_id(self.sam_tid).expect("Contig not in VCF list");
 					bcf_rec.clear();
 					bcf_rec.set_rid(rid); 
 					bcf_rec.set_pos(self.curr_x);
-					let t1 = Instant::now();		
-					let flt = write_fixed_columns(call, &vcf_output.hdr, filter_ids, &call_stats, &ref_context, bcf_rec)?;
-					timing.0 += Instant::now().duration_since(t1);
+					let flt = write_fixed_columns(call, filter_ids, tvec, &call_stats, &ref_context, bcf_rec)?;
 					let cpg = CPG_DISPLAY[cmp::max(CPG_STATE[called_gt[1]][called_gt[2]], CPG_STATE[called_gt[2]][called_gt[3]])];
-					let t1 = Instant::now();		
-					write_format_columns(call, filter_ids, &called_context, cpg, flt, &call_stats, bcf_rec)?;
-					timing.1 += Instant::now().duration_since(t1);
+					write_format_columns(call, filter_ids, &called_context, cpg, flt, tvec, &call_stats, bcf_rec)?;
 					bcf_rec.write(&mut vcf_output.file, &mut vcf_output.hdr)?;			
 					flt
 				} else { 0 };
@@ -379,19 +446,18 @@ pub fn write_vcf_entry(bs_cfg: Arc<BsCallConfig>, rx: mpsc::Receiver<WriteVcfJob
 	let mut vcf_output = bs_files.vcf_output.take().unwrap();
 	let filter_ids = get_filter_ids(&vcf_output.hdr);
 	let mut write_state: Option<WriteState> = None;
-	let mut timing = (Duration::new(0, 0), Duration::new(0,0));
 	loop {
 		match rx.recv() {
 			Ok(WriteVcfJob::Quit) => {
 				if let Some(ws) = write_state.as_mut() { 
-					if let Err(e) = ws.finish_block(&bs_cfg, &mut vcf_output, &filter_ids, &stat_tx, &mut timing) { error!("finish_block failed with error: {}", e); }
+					if let Err(e) = ws.finish_block(&bs_cfg, &mut vcf_output, &filter_ids, &stat_tx) { error!("finish_block failed with error: {}", e); }
 				}
 				break;
 			},
 			Ok(WriteVcfJob::CallBlock(block)) => {
 				debug!("Received new call block: {}:{}", block.sam_tid, block.start);
 				if let Some(ws) = write_state.as_mut() { 
-					if let Err(e) = ws.finish_block(&bs_cfg, &mut vcf_output, &filter_ids, &stat_tx, &mut timing) {
+					if let Err(e) = ws.finish_block(&bs_cfg, &mut vcf_output, &filter_ids, &stat_tx) {
 						error!("finish_block failed with error: {}", e);
 						break;
 					}
@@ -402,7 +468,7 @@ pub fn write_vcf_entry(bs_cfg: Arc<BsCallConfig>, rx: mpsc::Receiver<WriteVcfJob
 				match write_state.as_mut() {
 					Some(
 						ws) => {
-						if let Err(e) = ws.handle_calls(call_vec, &bs_cfg, &mut vcf_output, &filter_ids, &stat_tx, &mut timing) {
+						if let Err(e) = ws.handle_calls(call_vec, &bs_cfg, &mut vcf_output, &filter_ids, &stat_tx) {
 							error!("handle_calls failed with error: {}", e);
 							break;						
 						}
@@ -417,7 +483,5 @@ pub fn write_vcf_entry(bs_cfg: Arc<BsCallConfig>, rx: mpsc::Receiver<WriteVcfJob
 		}
 	}
 	info!("write_vcf thread shutting down");	
-	info!("Time for write_fixed_coloumns(): {}s", timing.0.as_secs_f64());
-	info!("Time for write_format_coloumns(): {}s", timing.1.as_secs_f64());
 }
 
