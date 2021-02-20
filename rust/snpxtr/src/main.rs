f32a96790cc2a2317a21b73abdc44e41f064c8da
Simon Heath <simon.heath@gmail.com>
2021-02-20 17:45:44 +0100

    Correct error message

--- rust/snpxtr/src/main.rs
+++ rust/snpxtr/src/main.rs
@@ -11,7 +11,7 @@ pub mod md5;
 pub mod tabix;
 
 fn main()  -> Result<(), String> {
-	let conf = cli::process_cli().map_err(|e| format!("dbsnp_index initialization failed with error: {}", e))?;
+	let conf = cli::process_cli().map_err(|e| format!("snpxtr_index initialization failed with error: {}", e))?;
 	match process::process(conf) {
 		Ok(_) => Ok(()),
 		Err(e) => {
