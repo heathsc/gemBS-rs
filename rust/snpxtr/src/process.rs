cad8c3a72cc9e4742ed14303e80a30e51caa670a
Simon Heath <simon.heath@gmail.com>
2021-05-04 08:28:58 +0200

    Correct printing of ref/alt het counts

--- rust/snpxtr/src/process.rs
+++ rust/snpxtr/src/process.rs
@@ -158,8 +158,8 @@ pub fn process(mut conf: Config) -> io::Result<()> {
                     match (gt[0], gt[1]) {
                         (2, 2) => write!(&mut buffer, "\t{}", get_hom(2, true))?,
                         (c1, c2) if c1 == c2 => write!(&mut buffer, "\t{}", get_hom(c1, false))?,
-                        (2, c2) => write!(&mut buffer, "\t{}", get_baf(0, c2))?,
-                        (c1, 2) => write!(&mut buffer, "\t{}", get_baf(0, c1))?,
+                        (2, c2) => write!(&mut buffer, "\t{}", get_baf(2, c2))?,
+                        (c1, 2) => write!(&mut buffer, "\t{}", get_baf(2, c1))?,
                         (c1, c2) => write!(&mut buffer, "\t{}", get_baf(c1, c2))?,
                     }    
                 } else { write!(&mut buffer, "\t{}", Baf::new(0, 0))? }
